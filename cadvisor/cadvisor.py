f04e563034d9c4ae4c5044875a2a2c06081e1934
Christopher McIntyre <me@cgmcintyre.com>
2016-08-19 21:17:33 +0100

    [container-info] Initial commit

--- cadvisor/cadvisor.py
+++ cadvisor/cadvisor.py
@@ -5,6 +5,7 @@ import requests
 
 import cadvisor.api as api
 from cadvisor.info.v1.machine import MachineInfo
+from cadvisor.info.v1.container import ContainerInfo
 
 class Cadvisor(object):
     def __init__(self, url):
@@ -17,3 +18,7 @@ class Cadvisor(object):
     def get_machine_info(self):
         data = api.get_json_data(api.machine_info_url(self.base_url))
         return MachineInfo(data)
+
+    def get_containers_info(self, name):
+        data = api.get_json_data(api.containers_info_url(self.base_url, name))
+        return ContainerInfo(name, data)
---
+++ cadvisor/info/v1/container.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+class ContainerInfo:
+    def __init__(self, name, dictionary):
+        self.name = name
