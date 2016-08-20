2a4eb590c498f046c93533799ab3a04eed2f6150
Christopher McIntyre <me@cgmcintyre.com>
2016-08-20 19:38:17 +0100

    [container-info] Removed superflous setting of attribute names

--- cadvisor/info/v1/container.py
+++ cadvisor/info/v1/container.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
+from collections import namedtuple
+
 from cadvisor.info.info import Info
 
 class ContainerInfo(Info):
@@ -14,3 +16,7 @@ class ContainerInfo(Info):
         self.load_attr('aliases')
         self.load_attr('namespace')
         self.load_attr('labels')
+
+ContainerReference = namedtuple('ContainerReference',
+                                'name id aliases namespace labels')
+ContainerReference.__new__.__defaults__ = (None,) * (len(ContainerReference._fields) - 1)
--- cadvisor/info/v1/machine.py
+++ cadvisor/info/v1/machine.py
@@ -9,60 +9,60 @@ class MachineInfo(Info):
         Info.__init__(self, dictionary, **kwargs)
 
     def setup(self):
-        self.load_attr('num_cores', attr='num_cores')
-        self.load_attr('cpu_frequency_khz', attr='cpu_frequency_khz')
-        self.load_attr('machine_id', attr='machine_id')
-        self.load_attr('system_uuid', attr='system_uuid')
-        self.load_attr('boot_id', attr='boot_id')
+        self.load_attr('num_cores')
+        self.load_attr('cpu_frequency_khz')
+        self.load_attr('machine_id')
+        self.load_attr('system_uuid')
+        self.load_attr('boot_id')
 
         self.load_attr_info_list('filesystems', FsInfo)
         self.load_attr_info_list('disk_map', DiskInfo)
         self.load_attr_info_list('network_devices', NetInfo)
         self.load_attr_info_list('topology', Node)
 
-        self.load_attr('cloud_provider', attr='cloud_provider')
-        self.load_attr('instance_type', attr='instance_type')
-        self.load_attr('instance_id', attr='instance_id')
+        self.load_attr('cloud_provider')
+        self.load_attr('instance_type')
+        self.load_attr('instance_id')
 
 class FsInfo(Info):
     def __init__(self, dictionary, **kwargs):
         Info.__init__(self, dictionary, **kwargs)
 
     def setup(self):
-        self.load_attr('device', attr='device')
-        self.load_attr('capacity', attr='capacity')
+        self.load_attr('device')
+        self.load_attr('capacity')
         self.load_attr('type', attr='fs_type')
-        self.load_attr('has_inodes', attr='has_inodes')
-        self.load_attr('inodes', attr='inodes')
+        self.load_attr('has_inodes')
+        self.load_attr('inodes')
 
 class DiskInfo(Info):
     def __init__(self, dictionary, **kwargs):
         Info.__init__(self, dictionary, **kwargs)
 
     def setup(self):
-        self.load_attr('name', attr='name')
-        self.load_attr('major', attr='major')
-        self.load_attr('minor', attr='minor')
-        self.load_attr('size', attr='size')
-        self.load_attr('scheduler', attr='scheduler')
+        self.load_attr('name')
+        self.load_attr('major')
+        self.load_attr('minor')
+        self.load_attr('size')
+        self.load_attr('scheduler')
 
 class NetInfo(Info):
     def __init__(self, dictionary, **kwargs):
         Info.__init__(self, dictionary, **kwargs)
 
     def setup(self):
-        self.load_attr('name', attr='name')
-        self.load_attr('mac_address', attr='mac_address')
-        self.load_attr('speed', attr='speed')
-        self.load_attr('mtu', attr='mtu')
+        self.load_attr('name')
+        self.load_attr('mac_address')
+        self.load_attr('speed')
+        self.load_attr('mtu')
 
 class Node(Info):
     def __init__(self, dictionary, **kwargs):
         Info.__init__(self, dictionary, **kwargs)
 
     def setup(self):
-        self.load_attr('node_id', attr='node_id')
-        self.load_attr('memory', attr='memory')
+        self.load_attr('node_id')
+        self.load_attr('memory')
         self.load_attr_info_list('filesystems', FsInfo)
         self.load_attr_info_list('caches', Cache)
         self.load_attr_info_list('cores', Core)
@@ -73,7 +73,7 @@ class Core(Info):
         Info.__init__(self, dictionary, **kwargs)
 
     def setup(self):
-        self.load_attr('core_id', attr='core_id')
+        self.load_attr('core_id')
         self.load_attr('thread_ids', attr='threads')
         self.load_attr_info_list('caches', Cache)
 
@@ -82,6 +82,6 @@ class Cache(Info):
         Info.__init__(self, dictionary, **kwargs)
 
     def setup(self):
-        self.load_attr('size', attr='size')
+        self.load_attr('size')
         self.load_attr('type', attr='cache_type')
-        self.load_attr('level', attr='level')
+        self.load_attr('level')
